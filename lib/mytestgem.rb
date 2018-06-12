c449f4cb182822baf5da7ef2cd6cb1668f3c720f
George Chen <1255187024@qq.com>
2018-06-12 17:00:53 +0800

    initial commit

---
+++ lib/mytestgem.rb
@@ -0,0 +1,8 @@
+require "mytestgem/version"
+
+module Mytestgem
+  # Your code goes here...
+  def self.hello
+    p "Hello from my gem"
+  end
+end
---
+++ lib/mytestgem/version.rb
@@ -0,0 +1,3 @@
+module Mytestgem
+  VERSION = "0.1.0"
+end
